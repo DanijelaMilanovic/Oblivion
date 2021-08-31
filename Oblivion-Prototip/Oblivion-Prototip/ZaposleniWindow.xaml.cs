7aa5477c303f579054385bfbed46766183ad4b78
DanijelaMilanovic <danijelamilanovic222@gmail.com>
2021-08-31 16:37:19 +0200

    Pocetna verzija zaposlenog

---
+++ Oblivion-Prototip/Oblivion-Prototip/ZaposleniWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Oblivion_Prototip
+{
+    /// <summary>
+    /// Interaction logic for ZaposleniWindow.xaml
+    /// </summary>
+    public partial class ZaposleniWindow : Window
+    {
+        public ZaposleniWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        /// <summary>
+        /// Logika izlaza iz aplikacije i omogućavanje promjene prikaza
+        /// </summary>
+        #region Upravljanje prikazom i izlazom iz aplikacije
+        private void ZaposleniWindow_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                WindowState = WindowState.Normal;
+                WindowStyle = WindowStyle.ThreeDBorderWindow;
+            }
+
+        }
+
+        private void ZaposleniWindow_StateChanged(object sender, EventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                WindowStyle = WindowStyle.None;
+            }
+        }
+        #endregion
+    }
+}
